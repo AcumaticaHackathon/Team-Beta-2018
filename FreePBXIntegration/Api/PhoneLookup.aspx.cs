cfc91ff6ab949de367233432f20e135b7496fd72
Jeremy Osterhouse <jeremy@onetencommunications.com>
2018-01-27 17:41:39 -0600

    Basic contact lookup api
    Get /Api/PhoneLookup.aspx?phone=5551234
    Returns the Display Name in an html page, for now

---
+++ FreePBXIntegration/Api/PhoneLookup.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using PX.Data;
+using PX.Objects.CR;
+using System.Web.UI.WebControls;
+
+public partial class Api_PhoneLookup : System.Web.UI.Page
+{
+    public String contact_name = "";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        ContactMaint graph = PXGraph.CreateInstance<ContactMaint>();
+        var search_phone = Request.QueryString["phone"];
+        var rows = PXSelect<Contact,
+            Where<Contact.phone1,
+                Equal<Required<Contact.phone1>>>>.Select(graph, search_phone);
+        foreach (Contact item in rows)
+        {
+            contact_name = item.DisplayName;
+        }
+
+    }
+}
